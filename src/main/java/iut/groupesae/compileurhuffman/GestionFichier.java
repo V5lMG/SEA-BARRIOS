8eac404ff85572cd623696bce4c2a41a84aa9b4e
V5lMG <114313700+V5lMG@users.noreply.github.com>
2024-04-28 11:16:12 +0200

    Premier commit

---
+++ src/main/java/iut/groupesae/compileurhuffman/GestionFichier.java
@@ -0,0 +1,11 @@
+package iut.groupesae.compileurhuffman;
+
+import static java.lang.System.out;
+
+public class GestionFichier {
+
+    public static void main(String[] args) {
+        // Démarrage du service
+        out.println("Le service est démarré.");
+    }
+}
